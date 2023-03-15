509583eb8fb53e5596e63287745ace10f3601499
Atif Bilal <itsatifkhan871@gmail.com>
2023-03-15 13:21:09 +0500

    updated

--- my_app/views.py
+++ my_app/views.py
@@ -11,18 +11,30 @@ def store(request):
 
 
 def cart(request):
-      if request.user.is_authenticated:
-            customer = request.user.customer
-            Order, created = Order.objects.get_or_create(customer=customer, complete=False)
-            items = Order.orderitem_set.all()
-      else:
-            items = []
-      context = {'items':items}
-      return render(request, 'store/cart.html', context)
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+        cart_item = order.get_cart_items
+    else:
+        items = []
+        cart_item = 0
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+    context = {'items': items, 'cart_item': cart_item}
+    return render(request, 'store/cart.html', context)
 
 
 def checkout(request):
-      context = {}
+      if request.user.is_authenticated:
+            customer = request.user.customer
+            order, created = Order.objects.get_or_create(customer=customer, complete=False)
+            items = order.orderitem_set.all()
+            cart_item = order.get_cart_items
+      else:
+            items = []
+            cart_item = 0
+            order = {'get_cart_total': 0, 'get_cart_items': 0}
+      context = {'items': items, 'cart_item': cart_item}
       return render(request, 'store/checkout.html', context)
 
 
